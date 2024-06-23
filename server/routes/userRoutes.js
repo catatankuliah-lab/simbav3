e15adc4566474a580581597dccc90c4a5fec7341
catatankuliah-lab <nanasupriatnakursus@gmail.com>
2024-06-23 17:31:36 +0700

    tambah git ignore

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Route untuk menambahkan mobil baru
 router.post('/add', userController.addUser);
 
-// Route untuk membaca semua data mobil
-router.get('/all', userController.getAllUser);
+router.get('/alls', userController.getAllUser);
 
 module.exports = router;
