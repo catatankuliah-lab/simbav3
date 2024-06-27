cb562aae049efc91024ee55375f0841302004a8a
aldoaltriansah <20180810048@uniku.ac.id>
2024-06-27 10:13:45 +0700

    edit server

--- server/server.js
+++ server/server.js
@@ -17,12 +17,12 @@ const picGUdangRoutes = require('./routes/picGudangRoutes');
 const checkerGudangRoutes = require('./routes/checkerGudangRoutes');
 const kantorCabangRoutes = require('./routes/kantorCabangRoutes');
 const gudangRoutes = require('./routes/gudangRoutes');
-const masterdatakpmRoutes = require("./routes/masterDataKpmRoutes");
-const woRoutes = require("./routes/woRoutes");
-const loRoutes = require("./routes/loRoutes");
-const doRoutes = require("./routes/doRoutes");
-const sjtRoutes = require("./routes/sjtRoutes");
-const itemWoRoutes = require("./routes/itemWoRoutes");
+const masterdatakpmRoutes = require('./routes/masterDataKpmRoutes');
+const woRoutes = require('./routes/woRoutes');
+const loRoutes = require('./routes/loRoutes');
+const doRoutes = require('./routes/doRoutes');
+const sjtRoutes = require('./routes/sjtRoutes');
+const itemWoRoutes = require('./routes/itemWoRoutes');
 const path = require("path");
 
 const app = express();
@@ -41,25 +41,25 @@ app.use(
 );
 
 // Menggunakan routes
-app.use("/api/alokasi", alokasiRoutes);
-app.use("/api/provinsi", provinsiRoutes);
-app.use("/api/kabupaten", kabupatenRoutes);
-app.use("/api/kecamatan", kecamatanRoutes);
-app.use("/api/desa", desaRoutes);
-app.use("/api/hakakses", hakAksesRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/adminkancab", adminKancabRoutes);
-app.use("/api/petugaspenyalur", petugasPenyalurRoutes);
-app.use("/api/picgudang", picGUdangRoutes);
-app.use("/api/checkergudang", checkerGudangRoutes);
-app.use("/api/kantorcabang", kantorCabangRoutes);
-app.use("/api/gudang", gudangRoutes);
-app.use("/api/masterdatakpm", masterdatakpmRoutes);
-app.use("/api/wo", woRoutes);
-app.use("/api/lo", loRoutes);
-app.use("/api/do", doRoutes);
-app.use("/api/sjt", sjtRoutes);
-app.use("/api/itemwo", itemWoRoutes);
+app.use('/api/alokasi', alokasiRoutes);
+app.use('/api/provinsi', provinsiRoutes);
+app.use('/api/kabupaten', kabupatenRoutes);
+app.use('/api/kecamatan', kecamatanRoutes);
+app.use('/api/desa', desaRoutes);
+app.use('/api/hakakses', hakAksesRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/adminkancab', adminKancabRoutes);
+app.use('/api/petugaspenyalur', petugasPenyalurRoutes);
+app.use('/api/picgudang', picGUdangRoutes);
+app.use('/api/checkergudang', checkerGudangRoutes);
+app.use('/api/kantorcabang', kantorCabangRoutes);
+app.use('/api/gudang', gudangRoutes);
+app.use('/api/masterdatakpm', masterdatakpmRoutes);
+app.use('/api/wo', woRoutes);
+app.use('/api/lo', loRoutes);
+app.use('/api/do', doRoutes);
+app.use('/api/sjt', sjtRoutes);
+app.use('/api/itemwo', itemWoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
